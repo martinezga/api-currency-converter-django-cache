49b9823de89af0b291fd24a8476d3dd78beaf9c5
Gabriella Martinez <52822208+martinezga@users.noreply.github.com>
2022-11-09 05:11:25 -0300

    Improved and added tests

--- tests/test_currencies/test_views.py
+++ tests/test_currencies/test_views.py
@@ -9,16 +9,28 @@ API_VERSION = '/v1'
 
 
 class TestCurrencyViews:
-    endpoint = f'{API_VERSION}/currencies/'
+    endpoint = f'{API_VERSION}/currencies'
 
     def test_list(self, api_client):
+        url = f'{self.endpoint}/'
         obj_expected = 5
         CurrencyFactory.create_batch(obj_expected)
 
         response = api_client().get(
-            self.endpoint,
+            url,
         )
         objs_received = json.loads(response.content).get('data').get('active_currencies')
 
         assert response.status_code == 200
         assert len(objs_received) == obj_expected
+
+    def test_to_implement(self, api_client):
+        url = f'{self.endpoint}/to_implement/'
+        # Improvement: think better test for this endpoint
+
+        response = api_client().get(
+            url,
+        )
+        objs_received = json.loads(response.content).get('data').get('currencies_to_implement')
+
+        assert response.status_code == 200
