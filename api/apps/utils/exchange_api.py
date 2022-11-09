382f6b8d59ffbaddf8a8f8e48c244e904596f6eb
Gabriella Martinez <52822208+martinezga@users.noreply.github.com>
2022-11-09 05:10:34 -0300

    Created connection with external exchange API

---
+++ api/apps/utils/exchange_api.py
@@ -0,0 +1,34 @@
+import requests
+from decouple import config
+
+
+class GetExchange:
+    def __init__(self, **kwargs):
+        self.url_base = 'https://openexchangerates.org/api'
+        self.headers = {'accept': 'application/json'}
+        self.api_key = config('EXCHANGE_API_KEY')
+
+    def get_exchange_rates(self):
+        endpoint = '/latest.json'
+        query_params = 'show_alternative=true'
+        url = f'{self.url_base}{endpoint}?{query_params}&app_id={self.api_key}'
+        exchange_rates = {}
+
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            exchange_rates = response.json()
+
+        return exchange_rates
+
+    def get_currencies(self):
+        endpoint = '/currencies.json'
+        # Improvement: show_inactive=true and compare with our data. We can create our historical
+        query_params = 'show_alternative=true&show_inactive=false'
+        url = f'{self.url_base}{endpoint}?{query_params}&app_id={self.api_key}'
+        currencies = {}
+
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            currencies = response.json()
+
+        return currencies
