b331900abfea19b7c49e75834895b47470c3bbd8
Gabriella Martinez <52822208+martinezga@users.noreply.github.com>
2022-11-10 14:54:25 -0300

    Added list available exchange currency rate

--- api/apps/exchanges/views/rates.py
+++ api/apps/exchanges/views/rates.py
@@ -2,6 +2,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
+from apps.exchanges.models import ExchangeRateModel
+from apps.exchanges.serializers import ExchangeRateSerializer
 from apps.utils.custom import CustomUtil
 
 
@@ -12,5 +14,13 @@ class ExchangeRateView(ViewSet):
         response = CustomUtil().response
         description = 'List all available exchange rates'
         response['request_detail']['description'] = description
+
+        objs = ExchangeRateModel.objects.filter(is_enabled=True)
+        serializer = ExchangeRateSerializer(objs, many=True)
+
+        response['status_code'] = 200
+        response['data'].pop('message')
+        response['data']['currencies_count'] = len(serializer.data)
+        response['data']['currency_rates'] = serializer.data
 
         return Response(response, status=response['status_code'])
